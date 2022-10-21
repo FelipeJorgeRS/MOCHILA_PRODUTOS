d69bbd9492bee9f1ed5a40aa575d871882271560
Felipe Jorge <felipe.jorge.2g@gmail.com>
2022-10-21 22:11:46 +0000

    Teste 21/10/2022 de Commit a partir do Replit

--- MOCHILA_PRODUTOS/src/Main.java
+++ MOCHILA_PRODUTOS/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Teste 2 de Commit a partir do GitHub");
 
+    System.out.println("Teste 21/10/2022 de Commit a partir do Replit");
 	}
 
 }
