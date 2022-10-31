21c4d85206a7bb12ce3f644150ad141cc25e3a30
Felipe Jorge <felipe.jorge.2g@gmail.com>
2022-10-30 21:52:23 -0300

    Update HeuristicaGulosa.java

--- MOCHILA_PRODUTOS/src/HeuristicaGulosa.java
+++ MOCHILA_PRODUTOS/src/HeuristicaGulosa.java
@@ -1,4 +1,26 @@
 
 public class HeuristicaGulosa {
+	
+	int i =0;
+	int capacidade =0;
+	
+	int [] hIdObjeto= new int[i];
+	int [] hCusto  = new int[i];
+	int [] hBeneficio  = new int[i];
+	int [] solucao= new int[capacidade];
+	
+	
+	for (int solucao = 0; solucao < capacidade.maxInt ; solucao=1) {
+		
+	
+			if (hIdObjeto(i).custo <= capacidade && hIdObjeto.beneficio.maxInt) {
 
-}
+					solucao(j) =1;
+			}// fim do if
+	
+			else{
+					solucao(j)=0;
+				}// fim do else
+		}// fim do for
+	
+}// fim da heuristica
